13b1139baab422121c099dd47747d2641a0e4cf4
ryoichi-obara <ryoichi-obara@users.noreply.github.com>
2019-01-08 20:45:29 +0900

    ES6

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const moment = require('moment')
-const fs = require('fs')
+const express = require('express');
+const bodyParser = require('body-parser');
+const moment = require('moment');
+const fs = require('fs');
+const { promisify } =  require('util');
 
-const fileOutputDestination = 'app.log'
+const fileOutputDestination = 'app.log';
+const portNo = 3000;
 
-var app = express()
+const app = express();
 
 // Initialize urlencoded/json() separably.
 // http://qiita.com/ktanaka117/items/596febd96a63ae1431f8
 app.use(bodyParser.urlencoded({
   extended: true
-}))
-app.use(bodyParser.json())
+}));
+app.use(bodyParser.json());
 
-app.post('/', function (req, res) {
-  var timeString = moment().format('YYYY/MM/DD HH:mm:ss.SSS')
-  console.log(timeString + ' Received.')
+app.post('/', async (req, res) => {
+  const timeString = moment().format('YYYY/MM/DD HH:mm:ss.SSS');
+  console.log(timeString + ' Received.');
 
-  fs.appendFileSync(fileOutputDestination,
-    timeString + '\n'
-      + JSON.stringify(req.headers) + '\n'
-      + JSON.stringify(req.body) + '\n'
-      + '\n'
+  await promisify(fs.appendFile)(
+    fileOutputDestination,
+    `${timeString}\n${JSON.stringify(req.headers)}\n${JSON.stringify(req.body)}\n\n`,
   ).catch(err => {
-    console.error(err)
-    res.status(500).send('500 Internal Server Error')
-  })
+    console.error(err);
+    res.status(500).send('500 Internal Server Error');
+  });
 
-  res.send('200 OK')
-})
+  res.send('200 OK');
+});
 
-app.listen(3000)
-console.log('Ready.')
+app.listen(portNo);
+console.log(`Ready, waiting connection on ${portNo}.`);
